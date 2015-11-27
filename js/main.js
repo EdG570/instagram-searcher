9281f570ecd97f1169dd8a55f1f5749a6dd5cb87
Ed Glazenski <edglazenski@gmail.com>
2015-11-27 11:41:46 -0500

    Foursquare api requests successfully being returned.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,56 @@
 (function(){
   var app = angular.module('myApp', ['ngMessages', 'ngAnimate']);
+            
 
   app.controller('searchCtrl', ['$scope', '$http', function($scope, $http){
 
-    $scope.validation = false;
+    $scope.loading = false;
+ 
+    
     //Checks form validation
     $scope.submit = function() {
+      $scope.loading = true;
       if($scope.myForm.$valid) {
         console.log('The form is valid!');
-        $scope.validation = true;
-        $scope.myForm.tag = '';
+        $scope.getJSONData();
+        
       } else {
         console.log('The form is invalid!');
+        
       }
     };
-  }]);
+
+    $scope.getJSONData = function() {
+      var url = "https://api.foursquare.com/v2/venues/explore";
+      var userLocation = $scope.userLocation;
+      var searchTag = $scope.searchTag;
+
+      var request = {
+        client_id: 'MFKB5Y1UIUJQ3S2G2YI3CD5EAGRPIODYIATL1FGLCXD1N1TF',
+        client_secret: '1VABGDFHKQ3BDAUOGYV4NJ5NL0B3XCQ2JRQ4MMIDKC3IGNVW',
+        near: userLocation,
+        query: searchTag,
+        v: '20140806',
+        m: 'foursquare'
+      };
+
+      $http({
+        method: 'GET',
+        url: url,
+        params: request,
+      })
+      .then(function(result){
+        $scope.groups = result.data.response.groups;
+        $scope.places = result.data.response.groups[0].items;
+        console.log($scope.groups);
+      },
+      function(result) {
+        alert('error');
+      });
+    };
+
+  //set loading=false and set searchTag=''
+
+  }]); 
+
 })();
